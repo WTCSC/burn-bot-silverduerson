58adc2b0f88967e0a48fa11ffc211e981bc206b1
silverduerson <silverduerson45@gmail.com>
2025-08-27 14:27:08 -0600

    Describe what you changed

---
+++ name = input ("what is your name?").py
@@ -0,0 +1,9 @@
+name = input ("what is your name?") 
+age = input("how old are you?")
+shoes = input("what shoes are you wearing?")
+holiday = input("what is your favortie holiday?")
+show = input("what is your favorite show?")
+
+print(f"""really {name} still watching {show} at {age} years old?
+also those {shoes} look like they have been through world war 2 and back.
+still celebrating {holiday} like a 5 year old""")
